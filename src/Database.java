a82c8899574ad2e008ee733dcbf469d1b832372b
Kamikaze_184 <alan.filho@hotmail.com>
2022-01-17 00:40:19 -0300

    Database getters e salvar para um arquivo

--- src/Database.java
+++ src/Database.java
@@ -4,39 +4,38 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Database {
-    private static String localClientes = "resources/clientes.txt";
-    private static Map<String, ArrayList> clientes = new HashMap<>();
+    private static String localClientes = "resources/clientes.ifbets";
+    private static Map<String, ArrayList<ArrayList<String>>> clientes = new HashMap<>(0);
 
-    public static void getUsuarios() throws IOException {
+    public static void carregarClientes() throws IOException {
         Path path = Path.of(localClientes);
         String s = Files.readString(path);
 
         String[] usuarios = s.split("\\|");
 
         for (String usuario : usuarios) {
-            usuario = usuario.replace("{", "");
-            usuario = usuario.replace("}", "");
-            String[] infoUsuario = usuario.split(",");
+            String[] infoUsuario = usuario.replace("{", "").replace("}", "").split(",");
 
-            ArrayList<String> valores = new ArrayList();
-            ArrayList<String> endereco = new ArrayList();
+            ArrayList<String> valores = new ArrayList<>();
+            ArrayList<String> endereco = new ArrayList<>();
 
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < infoUsuario.length; i++) {
                 String[] info = infoUsuario[i].split(":");
-                valores.add(info[1]);
-            }
 
-            for (int i = 3; i < infoUsuario.length; i++) {
-                String[] info = infoUsuario[i].split(":");
-                endereco.add(info[1]);
+                if (i < 3) {
+                    valores.add(info[1]);
+                } else {
+                    endereco.add(info[1]);
+                }
             }
 
             String key = valores.get(0).replace(" ", "");
 
             valores.remove(0);
-            ArrayList<ArrayList> data = new ArrayList<>();
+            ArrayList<ArrayList<String>> data = new ArrayList<>();
 
             data.add(valores);
             data.add(endereco);
@@ -45,7 +44,107 @@ public class Database {
         }
     }
 
-    public static ArrayList getCliente(String cpf) {
-        return clientes.get(cpf);
+    public static void salvarClientes() throws IOException {
+        Path path = Path.of(localClientes);
+
+        Set<Map.Entry<String, ArrayList<ArrayList<String>>>> entrySet = clientes.entrySet();
+        String stringClientes = "";
+        int count = 0;
+
+        for (Object entry : entrySet) {
+            String cliente = entry.toString();
+
+            String cpf = cliente.split("=")[0];
+            String[] info = cliente.split("=")[1].split("],");
+
+            String nome = info[0].replace("[[", "").split(",")[0];
+
+            while (nome.startsWith(" ")) {
+                nome = nome.substring(1);
+            }
+
+            String dataNascimento = info[0].replace("[[", "").split(",")[1];
+
+            while (dataNascimento.startsWith(" ")) {
+                dataNascimento = dataNascimento.substring(1);
+            }
+
+            String enderecoString = info[1].replace("[", "").replace("]", "");
+
+            while (enderecoString.startsWith(" ")) {
+                enderecoString = enderecoString.substring(1);
+            }
+
+            String[] endereco = enderecoString.split(",");
+
+            for (int i = 0; i < endereco.length; i++) {
+                while (endereco[i].startsWith(" ")) {
+                    endereco[i] = endereco[i].substring(1);
+                }
+            }
+
+            stringClientes += String.format(
+                    "{cpf:%s, nome:%s, dataDeNascimento:%s, endereco = logradouro:%s, cidade:%s, bairro:%s, rua:%s, numero:%s, uf:%s}",
+                    cpf, nome, dataNascimento, endereco[0], endereco[1], endereco[2], endereco[3], endereco[4],
+                    endereco[5]);
+
+            if (count < entrySet.size() - 1) {
+                stringClientes += "|";
+            }
+
+            count++;
+        };
+
+        Files.writeString(path, stringClientes);
     }
+
+    public static String getNomeCliente(String cpf) {
+        Object info = clientes.get(cpf).get(0);
+        String[] infoCliente = info.toString().replace("[", "").replace("]", "").split(",");
+        String nome = infoCliente[0];
+
+        while (nome.startsWith(" ")) {
+            nome = nome.substring(1);
+        }
+
+        return nome;
+    }
+
+    public static String getDataNascimentoCliente(String cpf) {
+        Object info = clientes.get(cpf).get(0);
+        String[] infoCliente = info.toString().replace("[", "").replace("]", "").split(",");
+        String dataNascimento = infoCliente[1];
+
+        while (dataNascimento.startsWith(" ")) {
+            dataNascimento = dataNascimento.substring(1);
+        }
+
+        return dataNascimento;
+    }
+
+    public static Map<String, String> getEnderecoCliente(String cpf) {
+        Object info = clientes.get(cpf).get(1);
+        String[] infoCliente = info.toString().replace("[", "").replace("]", "").split(",");
+
+        for (int i = 0; i < infoCliente.length; i++) {
+            while (infoCliente[i].startsWith(" ")) {
+                infoCliente[i] = infoCliente[i].substring(1);
+            }
+        }
+
+        Map<String, String> endereco = new HashMap<>(0) {
+            {
+                put("logradouro", infoCliente[0]);
+                put("cidade", infoCliente[1]);
+                put("bairro", infoCliente[2]);
+                put("rua", infoCliente[3]);
+                put("numero", infoCliente[4]);
+                put("uf", infoCliente[5]);
+            }
+        };
+
+        return endereco;
+
+    }
+
 }
--- src/Testes.java
+++ src/Testes.java
@@ -1,10 +1,22 @@
-import java.io.IOException;
+import java.util.Map;
 
 public class Testes {
-    public static void run() throws IOException{
-        //Testando metodos para ler o database
-        Database.getUsuarios();
+    public static void run() throws Exception {
+        Database.carregarClientes();
 
-        System.out.println(Database.getCliente("11122233300"));
+        System.out.println(Database.getNomeCliente("11122233300"));
+        System.out.println(Database.getDataNascimentoCliente("11122233300"));
+        Map<String, String> endereco = Database.getEnderecoCliente("11122233300");
+
+        System.out.println(endereco.get("logradouro"));
+        System.out.println(endereco.get("cidade"));
+        System.out.println(endereco.get("bairro"));
+        System.out.println(endereco.get("rua"));
+        System.out.println(endereco.get("numero"));
+        System.out.println(endereco.get("uf"));
+
+        //Falta criar os settrs
+
+        Database.salvarClientes();
     }
 }
