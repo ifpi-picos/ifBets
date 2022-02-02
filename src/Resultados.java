c032326d946eaf9cbdbb397d663392edd90c37a0
Kamikaze_184 <alan.filho@hotmail.com>
2022-02-02 11:22:28 -0300

    Pequena mudança nas notificações

--- src/Resultados.java
+++ src/Resultados.java
@@ -4,35 +4,41 @@ public class Resultados {
 
   public static void notificarVitoria(Aposta aposta) {
     Double ganho;
+    Double odd;
 
     if (aposta.getTimeEscolhido().equals(aposta.getJogo().getNomeTimeA())) {
       ganho = aposta.getValorApostado() * Database
           .getJogoByNome(aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB()).getValorVitoriaA();
-    } else if (aposta.getTimeEscolhido().equals(aposta.getJogo().getNomeTimeB())) {
-      ganho = aposta.getValorApostado() * Database
-          .getJogoByNome(aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB()).getValorVitoriaB();
+
+      odd = Database.getJogoByNome(aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB()).getValorVitoriaA();
     } else {
       ganho = aposta.getValorApostado() * Database
-          .getJogoByNome(aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB()).getValorEmpate();
+          .getJogoByNome(aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB()).getValorVitoriaB();
+      odd = Database.getJogoByNome(aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB()).getValorVitoriaB();
     }
 
     JOptionPane.showMessageDialog(null,
-        String.format("Parabéns %s, você acertou o resultado do jogo %s x %s, o %s venceu e você irá receber R$ %.2f",
+        String.format(
+            "Parabéns %s, você acertou o resultado do jogo %s x %s, o %s venceu e com uma aposta de R$ %.2f a %.2fx você irá receber R$ %.2f",
             aposta.getCliente().getNome(), aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB(),
-            aposta.getTimeEscolhido(), aposta.getValorApostado(), ganho));
+            aposta.getTimeEscolhido(), aposta.getValorApostado(), odd, ganho));
   }
 
   public static void notificarDerrota(Aposta aposta) {
+    String vencedor = aposta.getTimeEscolhido() == aposta.getJogo().getNomeTimeA() ? aposta.getJogo().getNomeTimeB()
+        : aposta.getJogo().getNomeTimeA();
+
     JOptionPane.showMessageDialog(null,
         String.format("Infelizmente %s, você errou o resultado do jogo %s x %s, o %s venceu e você perdeu R$ %.2f",
             aposta.getCliente().getNome(), aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB(),
-            aposta.getTimeEscolhido(), aposta.getValorApostado()));
+            vencedor, aposta.getValorApostado()));
   }
 
   public static void notificarEmpate(Aposta aposta) {
     JOptionPane.showMessageDialog(null,
         String.format(
             "Infelizmente %s, você errou o resultado do jogo %s x %s, O resultado foi um empate e você perdeu R$ %.2f",
-            aposta.getCliente().getNome(), aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB(), aposta.getValorApostado()));
+            aposta.getCliente().getNome(), aposta.getJogo().getNomeTimeA(), aposta.getJogo().getNomeTimeB(),
+            aposta.getValorApostado()));
   }
 }
